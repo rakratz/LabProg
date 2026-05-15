31fd57d44d2391df800ff8526f18ce5501e578b4
Prof. Me. Ricardo de Andrade Kratz <rakratz@gmail.com>
2026-05-15 08:36:44 -0300

    Update leitor.c

--- Arquivo/leitor.c
+++ Arquivo/leitor.c
@@ -6,7 +6,7 @@ void salvaArquivo(char *nomeArq, int *num1, int *num2)
     FILE *arquivo = fopen(nomeArq, "w"); // "w" cria o arquivo ou limpa se exisitir (write)
     if (arquivo == NULL)
     {
-        printf("Erro ao salvar o arquivo $s!\n", nomeArq);
+        printf("Erro ao salvar o arquivo %s!\n", nomeArq);
     }
     // Escrever os dois valores separados por um espaço
     fprintf(arquivo, "%d %d", *num1, *num2);
@@ -19,7 +19,7 @@ void lerDoisNumeros(char *nomeArq, int *num1, int *num2)
     FILE *arquivo = fopen(nomeArq, "r"); // "r" apenas leitura (read)
     if (arquivo == NULL)
     {
-        printf("Erro ao salvar o arquivo $s!\n", nomeArq);
+        printf("Erro ao abrir o arquivo %s!\n", nomeArq);
     }
     fscanf(arquivo, "%d %d", num1, num2);
     fclose(arquivo);
